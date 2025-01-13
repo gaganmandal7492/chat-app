07985227a95c42cebb432c6ead4b3182ade2a519
Gagan Mandal <mandalgagan4200@gmail.com>
2025-01-13 10:05:32 +0530

    add client files

---
+++ client/src/modules/Form/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='bg-white w-[450px] h-[500px] shadow-lg rounded-lg'>
+       
+    </div>
+  )
+}
+
+export default Form;
